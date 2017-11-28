59f8618df18e1f1a9ab8dfa5a22c2547493aa82b
Nikolas Bogucarskis <niktechnopro@gmail.com>
2017-11-27 21:40:48 -0500

    final commit

---
+++ images/Pomodoro Clock_files/scripts.js
@@ -0,0 +1,11 @@
+// Add ready listener to the document.
+// I.e., JavaScript, wait until the DOM is fully loaded before proceeding to this code
+
+	console.log("f")
+$(document).ready(function(){
+	// console.log("Sanity Check!!")
+
+
+
+
+});
